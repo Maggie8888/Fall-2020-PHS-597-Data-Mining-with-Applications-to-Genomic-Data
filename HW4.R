35fe83619bb2f6a694a5b7433d8260e85a2bb0a4
Maggie8888 <45131108+Maggie8888@users.noreply.github.com>
2020-09-28 00:29:24 -0400

    Add files via upload

--- HW4.R
+++ HW4.R
@@ -83,6 +83,7 @@ print((group.means - matrix(1, ng, 1) %*% t(xbar)) %*% scaling2)
 # as recovered using `predict.lda()`
 lda.model <- lda(X, grouping = y)
 print(predict(lda.model, as.data.frame(lda.model$means))$x)
+predict(lda.model,X)$class
 
 # center data around weighted means & transform
 means <- colSums(lda.model$prior * lda.model$means)
@@ -92,4 +93,40 @@ mod <- scale(X, center = means, scale = FALSE) %*%
 plot.df <- data.frame(mod, "Outcome" = y)
 library(ggplot2)
 ggplot(plot.df, aes(x = LD1, y = LD2, color = Outcome)) + geom_point()
+################## QDA ######################
+QuaDA<- function(Input,Response){
+  Input=as.matrix(Input)
+  N=nrow(Input)
+  p=ncol(Input)
+  K=length(unique(Response))
+  classes=as.numeric(levels(as.factor(Response)))
+  Mu_k=matrix(0,nrow = p,ncol = K)#centroid vectors
+  Pi_k=vector(length= K)# classes a priori
+  sigma_k=list()#data covariances matrix per class
+  sigmaMinus1_k=list()
+  det_k=vector(length = K)
+  for (i in 1:K){
+    sigma_k[[i]]=matrix(0,ncol=p,nrow = p)
+    tmp=Input[Response==classes[i],]
+    N0=nrow(tmp)
+    Pi_k[i]=N0/N
+    Mu_k[,i]=colMeans(tmp)
+    for(j in 1:N0){
+      sigma_k[[i]]=sigma_k[[i]]+(tmp[j,]-Mu_k[,i])%*%t(tmp[j,]-Mu_k[,i])
+    }
+    sigma_k[[i]]=(1/(N0-1))*sigma_k[[i]]
+    sigmaMinus1_k[[i]]=solve(sigma_k[[i]])
+    det_k[i]=det(sigma_k[[i]])
+  }
+  #discriminant matrix
+  deltaTrain=matrix(0,ncol = K,nrow = N)
+  for (i in 1:K){
+    deltaTrain[,i]=apply(Input,1,function(x) -0.5*t(x-Mu_k[,i])%*%sigmaMinus1_k[[i]]%*%(x-Mu_k[,i]))
+    deltaTrain[,i]=deltaTrain[,i]-0.5*log(det_k[i])+log(Pi_k[i])
+  }
+  #prediction
+  yHatTrain=apply(deltaTrain,1,which.max)
+  return(list(yhat=yHatTrain,sigmaMinus1_k=sigmaMinus1_k,Mu_k=Mu_k,det_k=det_k,Pi_k=Pi_k))
+}
 
+QuaDA(X,y)
